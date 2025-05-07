8bdd081dea21a92de4daffac860516c74d37f075
Brandt Newton <brandtnewton@google.com>
2025-05-07 13:57:32 -0400

    changed link format

--- kafka-connect-bigtable-sink/sink/third_party/apache_hbase_common/src/main/java/com/google/cloud/kafka/connect/bigtable/utils/ByteUtils.java
+++ kafka-connect-bigtable-sink/sink/third_party/apache_hbase_common/src/main/java/com/google/cloud/kafka/connect/bigtable/utils/ByteUtils.java
@@ -36,9 +36,11 @@ import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 
 /**
- * This code is copied from <a
- * href="https://github.com/apache/hbase/blob/master/hbase-common/src/main/java/org/apache/hadoop/hbase/util/Bytes.java">hbase-common</a>
- * using Apache 2.0 License. This code has been modified to remove functions that we don't need.
+ * This code is copied from hbase-common using Apache 2.0 License. This code has been modified to
+ * remove functions that we don't need.
+ *
+ * @see <a
+ *     href="https://github.com/apache/hbase/blob/master/hbase-common/src/main/java/org/apache/hadoop/hbase/util/Bytes.java">Bytes.java</a>
  */
 public class ByteUtils {
 
